b60acf48263eab0e88021edd58cd50498211736f
Abrar Shivani <ashivani@hawk.iit.edu>
2016-11-24 20:59:40 -0800

    collect stub

--- a4/collect.py
+++ a4/collect.py
@@ -1,3 +1,23 @@
 """
 collect.py
 """
+
+from collections import Counter, defaultdict
+
+
+def authenticate():
+    pass
+
+def store_comment():
+    pass
+
+def get_comments(service, store_comment):
+    pass
+
+def main():
+    service = authenticate()
+    keywords = []
+    comments = get_comments(service, store_comment)
+
+if __name__ == '__main__':
+    main()
