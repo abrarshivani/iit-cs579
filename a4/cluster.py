976107d53505d9836c7a24a2f50587430c24328e
Abrar Shivani <ashivani@hawk.iit.edu>
2016-11-29 15:22:59 -0800

    collect.py and cluster.py basics

--- a4/cluster.py
+++ a4/cluster.py
@@ -1,3 +1,92 @@
 """
 cluster.py
 """
+
+import re
+import string
+
+filename = "comments"
+
+
+def build_slang_dict(filename):
+    slang = {}
+    lines = []
+    with open(filename, "r") as handle:
+        lines = handle.readlines()
+    for line in lines:
+        words = line.split("\n")[0].split("  -   ")
+        slang[words[0]] = words[1]
+    return slang
+
+def remove_extra_whitespaces(text):
+    text = re.sub('[\s]+', ' ', text)
+    return text
+
+def gethashtags(text):
+    hashtag_regex = '#([^\s]+)'
+    hashtags = re.findall(hashtag_regex, text)
+    return hashtags
+
+
+def replace_hashtag_word(text):
+    text = re.sub(r'#([^\s]+)', r'\1', text)
+    return text
+
+
+def remove_usernames(text):
+    text = re.sub('@[^\s]+', ' ', text)
+    return text
+
+
+def remove_non_word_chars(text):
+    text = re.sub('[^A-Za-z\s]*', '', text)
+    return text
+
+
+def replace_dict_def(text, db):
+    words = text.split(" ")
+    text = ""
+    for word in words:
+        try:
+            meaning = db[word]
+            text = text + meaning + " "
+        except:
+            text = text + word + " "
+    return text
+
+
+def strip_punctuation(tweet):
+    tweet = "".join(c for c in tweet if c not in string.punctuation)
+    return tweet
+
+
+def preprocess_comment(comment, db):
+    #comment = comment.encode('ascii', 'ignore')
+    comment = replace_hashtag_word(comment)
+    comment = remove_usernames(comment)
+    comment = strip_punctuation(comment)
+    comment = replace_dict_def(comment, db)
+    comment = remove_non_word_chars(comment)
+    comment = remove_extra_whitespaces(comment)
+    return comment
+
+
+def preprocess_comments(comments, db):
+    result = None
+    for comment in comments:
+        comment = preprocess_comment(comment, db)
+    return comment
+
+
+def main():
+    comments = ["This is lol"]
+    slang_dict = build_slang_dict("slang.txt")
+    #comments = get_comments_from_file()
+    preprocessed_comments = preprocess_comments(comments, slang_dict)
+    print(preprocessed_comments)
+    #classified_comments = classify_comments(preprocessed_comments)
+    #store_results(classified_comments)
+
+
+if __name__ == '__main__':
+    main()
--- a4/collect.py
+++ a4/collect.py
@@ -3,21 +3,48 @@ collect.py
 """
 
 from collections import Counter, defaultdict
+from TwitterAPI import TwitterAPI
+import time
+import pickle
+import signal
+
+#Filename
+comments_data = "comments"
+
+# Service Tokens
+consumer_key = 'uJQx8OkEByWaMaEucxyJl0t8D'
+consumer_secret = 'SEbqZRSDqn37P2uGrXxWFQTLkbltsRNv2p4cZtV5eUzLJHdZcN'
+access_token = '771910893881823233-KkWesZ1Li83LQcwIRDPqB7EoYI4RZGB'
+access_token_secret = 'tlS3LpIG0L1JbcWVyXfSD3HB6FpWl2EOyvSL7etfCWOPL'
 
 
 def authenticate():
-    pass
+    return TwitterAPI(consumer_key, consumer_secret, access_token, access_token_secret)
 
-def store_comment():
-    pass
 
-def get_comments(service, store_comment):
-    pass
+def store_comment(comment, comments_file):
+    print(comment['text'])
+    pickle.dump(comment, comments_file)
+
+
+def get_comments(service, keywords, store_comment_fnc, comments_file):
+    comments = service.request('statuses/filter', {'language': 'en', 'track': keywords})
+    for comment in comments:
+        try:
+            store_comment_fnc(comment, comments_file)
+        except:
+            pass
+
 
 def main():
     service = authenticate()
-    keywords = []
-    comments = get_comments(service, store_comment)
+    comments_file = open(comments_data, "wb")
+    keywords = ['thanksgiving']
+    try:
+        get_comments(service, keywords, store_comment, comments_file)
+    except KeyboardInterrupt:
+        print("Keyboard Interrupt")
+        comments_file.close()
 
 if __name__ == '__main__':
-    main()
+        main()
